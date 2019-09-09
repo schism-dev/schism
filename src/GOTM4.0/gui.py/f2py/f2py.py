dcfe896d2b01b389de116c242a88d790f2b7d456
Joseph Zhang <yjzhang@vims.edu>
2019-09-09 14:14:30 -0400

    rm GOTM4

--- src/GOTM4.0/gui.py/f2py/f2py.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/python
-
-import numpy.f2py
-r=numpy.f2py.f2py2e.main()
--- src/GOTM4.0/src/airsea/tst_airsea.F90
+++
@@ -1,8 +0,0 @@
-use airsea
-
-call air_sea_interaction(0,0)
-end
-
-!-----------------------------------------------------------------------
-! Copyright by the GOTM-team under the GNU Public License - www.gnu.org
-!----------------------------------------------------------------------- 
