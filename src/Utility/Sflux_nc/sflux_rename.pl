5888135ea42fa91f1084a98c5d334bcbfd92afae
Joseph Zhang <yjzhang@vims.edu>
2019-10-02 14:36:20 -0400

    Fixed an issue in sflux renaming script (self looping)

--- src/Utility/Sflux_nc/sflux_rename.pl
+++ src/Utility/Sflux_nc/sflux_rename.pl
@@ -6,10 +6,12 @@
 opendir(DIR, ".") or die "can't opendir: $!";
 while (defined($file = readdir(DIR))) {
   if($file =~ "sflux_" && $file =~ ".nc") {
-    print "doing $file\n";
     ($a,$b,$c)=split("[.]",$file);
     #print "$a.0$b.$c\n";
-    system "ln -sf $file $a.0$b.$c";
+    if(length($b)==3) {
+      print "doing $file\n";
+      system "ln -sf $file $a.0$b.$c";
+    }
   }
 }
 closedir(DIR);
