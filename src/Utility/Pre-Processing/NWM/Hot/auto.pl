e2a140aec301d2e91ec4364635227c27e70135e6
Fei Ye <feiye@vims.edu>
2019-12-20 01:28:37 -0500

    Updated pre-processing scripts for NWM (NOAA project). Added interpolate_unstructured to the repo to further automate things. Split the scripts for 3Dth and hotstart. Minor changes on the hotstart scripts.

--- src/Utility/Pre-Processing/NWM/Hot/auto.pl
+++ src/Utility/Pre-Processing/NWM/Hot/auto.pl
@@ -5,8 +5,8 @@ use File::Copy qw(copy);
 use File::Copy qw(move);
 use Cwd;
 
-print("You may need to recompile 'gen_source2.f90'\n");
-print("e.g: ifort -O2 -mcmodel=medium -assume byterecl -o gen_hot_3Dth_from_hycom gen_hot_3Dth_from_hycom.f90 ../UtilLib/compute_zcor.f90 -I$NETCDF/include -I$NETCDF_FORTRAN/include -L$NETCDF_FORTRAN/lib -L$NETCDF/lib -lnetcdf -lnetcdff\n\n\n");
+print("You may need to recompile 'gen_source2.f90',\n");
+print("see an example compiling cmd in the source code.\n");
 print("Make sure you have netcdf libraries\n");
 #{
 #  local( $| ) = ( 1 );
@@ -32,8 +32,27 @@ system("./auto.pl");
 
 
 chdir($thisdir);
+
 #set elev.ic
 system("ln -sf Elev_IC/elev.ic DB_elev_ic.gr3");
+
+#set salt.ic
+print("Interpolating salinity i.c. in Delaware Bay\n");
+system("ln -sf DB_surf_S_ic.subset.gr3 bg.gr3");
+system("rm fg.gr3"); 
+#make fg.gr3, to be interpolated on
+system("./gen_gr3.pl");
+#set initial sal to 0 in DB before interpolation
+system("$script_dir/auto_edit_region 0 DB.reg fg.gr3 0");
+move("out.gr3","fg.gr3");
+#make include.gr3 for salt interp
+system("$script_dir/auto_edit_region 0 DB.reg hgrid.gr3 1 0");
+move("out.gr3","include.gr3");
+#do interp between 2 *.gr3
+system("$script_dir/interpolate_unstructured");
+system("mv fg.new DB_surf_S_ic.gr3");
+print("Done interpolating salinity i.c. in Delaware Bay\n");
+
 #set CB=1 and DB=2 in estuary.gr3
 system("$script_dir/auto_edit_region 0 CB.reg hgrid.gr3 1 0");
 move("out.gr3","estuary.gr3");
@@ -44,9 +63,8 @@ move("out.gr3","estuary.gr3");
 system("./gen_hot_3Dth_from_hycom");
 
 unlink("../hotstart.nc");
-unlink("../*D.th.nc");
+unlink("./*D.th.nc");
 
 copy("hotstart.nc","../hotstart.nc");
-copy("*D.th.nc","../");
 
 print("Done.\n")
