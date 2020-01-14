a35bcd92f693ed361d5a251a0514e4bee3ef49f7
Joseph Zhang <yjzhang@vims.edu>
2020-01-14 14:52:59 -0500

    Add 2 scripts for global lon/lat grids

---
+++ src/Utility/Grid_Scripts/check_lonlat_grid.f90
@@ -0,0 +1,169 @@
+! Check quality of lon/lat grid (including global with poles), b/cos
+! gredit has issues (e.g. hi distortion near poles), using SCHISM's 3D coordinates on an ellipsoid.
+! Fix negative elements and output list of skew elem.
+!
+! Inputs: screen; hgrid.ll (tri only)
+! Outputs: hgrid.ll.new; fort.99 (diagnostics)
+
+! ifort -O2 -mcmodel=medium -CB -Bstatic -o check_lonlat_grid check_lonlat_grid.f90
+
+  implicit real*8(a-h,o-z)
+  real*8, parameter :: pi=3.1415926d0
+  real*8, parameter :: deg2rad=pi/180.d0
+  real*8, parameter :: rad2deg=180.d0/pi
+  real*8, parameter :: rearth_eq=6378206.4 ![m]
+  real*8, parameter :: rearth_pole=6378206.4
+
+  real*8 :: xx(4),yy(4),swild(4)
+  integer, allocatable :: i34(:),elnode(:,:),i34_new(:),elnode_new(:,:)
+  real*8, allocatable :: xlon(:),ylat(:),dp(:),area(:),quad_loc(:,:), &
+ &xctr(:),yctr(:),xnd(:),ynd(:),znd(:),pframe(:,:,:)
+
+  print*, 'Input max allowable skewness for triangles:'
+  read*, skew_max
+  
+  open(14,file='hgrid.ll',status='old')
+  read(14,*); read(14,*)ne,np
+
+  allocate(xlon(np),ylat(np),dp(np),area(ne),i34(ne),elnode(4,ne), &
+ &i34_new(2*ne),elnode_new(4,2*ne),quad_loc(2,ne),xctr(ne),yctr(ne),xnd(np),ynd(np),znd(np), &
+ &pframe(3,3,np))
+
+  do i=1,np
+    read(14,*) j,xlon(i),ylat(i),dp(i)
+    xtmp=xlon(i)*deg2rad !to rad
+    ytmp=ylat(i)*deg2rad !to rad
+    xnd(i)=rearth_eq*cos(ytmp)*cos(xtmp)
+    ynd(i)=rearth_eq*cos(ytmp)*sin(xtmp)
+    znd(i)=rearth_pole*sin(ytmp)
+  enddo !i
+
+  !Compute pframe
+  do i=1,np
+    xtmp=xlon(i)*deg2rad !to rad
+    ytmp=ylat(i)*deg2rad
+    pframe(1,1,i)=-sin(xtmp) !zonal dir.
+    pframe(2,1,i)=cos(xtmp)
+    pframe(3,1,i)=0
+    pframe(1,2,i)=-cos(xtmp)*sin(ytmp) !meri. dir.
+    pframe(2,2,i)=-sin(xtmp)*sin(ytmp)
+    pframe(3,2,i)=cos(ytmp)
+    call cross_product(pframe(1,1,i),pframe(2,1,i),pframe(3,1,i), &
+   &pframe(1,2,i),pframe(2,2,i),pframe(3,2,i),pframe(1,3,i),pframe(2,3,i),pframe(3,3,i))
+  enddo !i=1,npa
+
+  do i=1,ne
+    read(14,*) j,i34(i),elnode(1:i34(i),i)
+    if(i34(i)/=3) stop 'No quads plz'
+  enddo !i
+  close(14)  
+  
+  i34_new(1:ne)=i34
+  elnode_new(:,1:ne)=elnode
+
+  do i=1,ne
+    !Project to a local plane @ node 1
+    nd1=elnode(1,i)
+    do j=1,i34(i)
+      nd=elnode(j,i)
+      call project_pt('g2l',xnd(nd),ynd(nd),znd(nd),(/xnd(nd1),ynd(nd1),znd(nd1)/),pframe(:,:,nd1),xx(j),yy(j),tmp)
+    enddo !j
+
+    do j=1,i34(i)
+      j1=j+1
+      if(j1>i34(i)) j1=j1-i34(i)
+      swild(j)=sqrt((xx(j)-xx(j1))**2+(yy(j)-yy(j1))**2) !side length
+    enddo !j
+
+    n1=elnode(1,i)
+    n2=elnode(2,i)
+    n3=elnode(3,i)
+    area(i)=signa(xx(1),xx(2),xx(3),yy(1),yy(2),yy(3))
+
+    if(i34(i)==3) then !tria
+      ifl=0
+      if(area(i)<0) then
+        write(*,*)'Fixing negative area at elem:',i,area(i)
+        elnode_new(1,i)=elnode(2,i)
+        elnode_new(2,i)=elnode(1,i)
+        area(i)=-area(i)
+      else if(area(i)==0) then
+        ifl=1
+        write(99,*)'Elem_has_0_area:',i,xlon(i),ylat(i)
+      endif
+
+      !Skewness
+      if(ifl==0) then
+        skew=maxval(swild(1:3))/sqrt(area(i)/pi)
+        if(skew>skew_max) write(99,*)'Skew_elem:',i,xlon(i),ylat(i)
+      endif !ifl
+    else if(i34(i)==4) then !quad
+      
+    else 
+      stop 'unknown elem type'
+    endif !i34(i)
+  enddo !i=1,ne
+
+! Output
+  open(12,file='hgrid.ll.new',status='replace')
+  write(12,*)
+  write(12,*)ne,np
+  do i=1,np
+    write(12,*)i,real(xlon(i)),real(ylat(i)),real(dp(i))
+  enddo !i
+  do i=1,ne
+    write(12,*)i,i34_new(i),elnode_new(1:i34_new(i),i)
+  enddo !i
+  close(12)
+
+  print*, 'finished'
+
+  stop
+  end
+
+
+      function signa(x1,x2,x3,y1,y2,y3)
+!...  Compute signed area formed by pts 1,2,3
+      implicit real*8(a-h,o-z)
+
+      signa=((x1-x3)*(y2-y3)-(x2-x3)*(y1-y3))/2
+
+      return
+      end
+
+      subroutine project_pt(dir,xi,yi,zi,origin0,frame0,xo,yo,zo)
+      implicit real*8(a-h,o-z)
+
+      character(len=3), intent(in) :: dir
+      real(8), intent(in) :: xi,yi,zi,origin0(3),frame0(3,3)
+      real(8), intent(out) :: xo,yo,zo
+
+      !Local
+      real(8) :: wild(3)
+
+      if(dir.eq.'g2l') then
+        wild(1:3)=(xi-origin0(1))*frame0(1,1:3)+(yi-origin0(2))*frame0(2,1:3)+ &
+                 &(zi-origin0(3))*frame0(3,1:3)
+      else if(dir.eq.'l2g') then
+        wild(1:3)=origin0(1:3)+xi*frame0(1:3,1)+yi*frame0(1:3,2)+ &
+     &zi*frame0(1:3,3)
+      else
+        stop 'PROJECT_PT: unknown flag'
+      endif
+      xo=wild(1)
+      yo=wild(2)
+      zo=wild(3)
+
+      end subroutine project_pt
+
+      subroutine cross_product(x1,y1,z1,x2,y2,z2,x3,y3,z3)
+      implicit real*8(a-h,o-z)
+      real(8),intent(in) :: x1,y1,z1,x2,y2,z2
+      real(8),intent(out) :: x3,y3,z3
+
+      x3=y1*z2-y2*z1
+      y3=x2*z1-x1*z2
+      z3=x1*y2-x2*y1
+
+      end subroutine cross_product
+
---
+++ src/Utility/Grid_Scripts/gen_boundary_global.f90
@@ -0,0 +1,309 @@
+! Analyze global grid, assuming north pole is inside grid, and the only exteriod
+! land bnd is in the Southern Ocean.
+! Inputs: node index below; hgrid.ll (tri only)
+! Outputs: bnd.out (b.c. part of hgrid.ll)
+! ifort -Bstatic -O2 -mcmodel=medium -o gen_boundary_global gen_boundary_global.f90
+
+  implicit real*8(a-h,o-z)
+  integer :: nx(4,4,3)
+  real*8, allocatable :: xnd(:),ynd(:),dp(:),area(:)
+  integer, allocatable :: elnode(:,:),i34(:),nne(:),indel(:,:),ic3(:,:), &
+  &icolor(:),icolor2(:),ibnd(:),isdel(:,:),elside(:,:),isidenode(:,:),ilnd(:)
+   
+  !Input a node on Antarctica bnd:
+  node_ant=70665
+
+  open(14,file='hgrid.ll',status='old')
+  read(14,*); read(14,*)ne,np
+  allocate(xnd(np),ynd(np),dp(np),area(ne),i34(ne),elnode(3,ne),nne(np),ic3(3,ne), &
+ &icolor(np),icolor2(np),ibnd(np),ilnd(np))
+  do i=1,np
+    read(14,*)j,xnd(i),ynd(i),dp(i)
+  enddo !i
+  i34=3
+  do i=1,ne
+    read(14,*)j,k,elnode(:,i)
+
+!    n1=elnode(1,i); n2=elnode(2,i); n3=elnode(3,i);
+!    ar1=signa(xnd(n1),xnd(n2),xnd(n3),ynd(n1),ynd(n2),ynd(n3))
+!    if(ar1<=0) then
+!      print*, 'area<=0:',i,ar1
+!    endif
+!    area(i)=ar1
+!
+!    if(area(i)<=0) then
+!      write(*,*)i,area(i)
+!      stop
+!    endif
+  enddo !i=1,ne
+  close(14)
+ 
+! Compute geometry
+  do k=3,4
+    do i=1,k
+      do j=1,k-1
+        nx(k,i,j)=i+j
+        if(nx(k,i,j)>k) nx(k,i,j)=nx(k,i,j)-k
+        if(nx(k,i,j)<1.or.nx(k,i,j)>k) then
+          write(*,*)'nx wrong',i,j,k,nx(k,i,j)
+          stop
+        endif
+      enddo !j
+    enddo !i
+  enddo !k
+
+  nne=0
+  do i=1,ne
+    do j=1,i34(i)
+      nd=elnode(j,i)
+      nne(nd)=nne(nd)+1
+!      indel(nne(nd),nd)=i
+    enddo
+  enddo
+  mnei=maxval(nne)
+
+  allocate(indel(mnei,np),stat=istat)
+  if(istat/=0) stop 'Failed to alloc. indel'
+  nne=0
+  do i=1,ne
+    do j=1,3
+      nd=elnode(j,i)
+      nne(nd)=nne(nd)+1
+      if(nne(nd)>mnei) then
+        write(*,*)'Too many neighbors',nd
+        stop
+      endif
+      indel(nne(nd),nd)=i
+    enddo
+  enddo !i
+
+! Compute ball info; this won't be affected by re-arrangement below
+  do i=1,ne
+    do j=1,3
+      ic3(j,i)=0 !index for bnd sides
+      nd1=elnode(nx(i34(i),j,1),i)
+      nd2=elnode(nx(i34(i),j,2),i)
+      do k=1,nne(nd1)
+        ie=indel(k,nd1)
+        if(ie/=i.and.(elnode(1,ie)==nd2.or.elnode(2,ie)==nd2.or.elnode(3,ie)==nd2)) ic3(j,i)=ie
+      enddo !k
+    enddo !j
+  enddo !i
+
+! Sides
+  ns0=0
+  do ie=1,ne
+    do j=1,3 !visit each side associated with element ie
+      nd1=elnode(nx(i34(ie),j,1),ie)
+      nd2=elnode(nx(i34(ie),j,2),ie)
+
+      if(ic3(j,ie)==0.or.ie<ic3(j,ie)) ns0=ns0+1
+    enddo !j
+  enddo !i
+
+  allocate(elside(3,ne),isdel(2,ns0),isidenode(2,ns0))
+
+  ns=0 !# of sides
+  do i=1,ne
+    do j=1,i34(i)
+      nd1=elnode(nx(i34(i),j,1),i)
+      nd2=elnode(nx(i34(i),j,2),i)
+      if(ic3(j,i)==0.or.i<ic3(j,i)) then !new sides
+        ns=ns+1
+        if(ns>ns0) then
+          write(*,*)'Too many sides'
+          stop
+        endif
+        elside(j,i)=ns
+        isdel(1,ns)=i
+        isidenode(1,ns)=nd1
+        isidenode(2,ns)=nd2
+        !xcj(ns)=(xnd(nd1)+xnd(nd2))/2
+        !ycj(ns)=(ynd(nd1)+ynd(nd2))/2
+
+        isdel(2,ns)=ic3(j,i) !bnd element => bnd side
+!       Corresponding side in element ic3(j,i)
+        if(ic3(j,i)/=0) then !old internal side
+          iel=ic3(j,i)
+          index=0
+          do k=1,i34(iel)
+            if(ic3(k,iel)==i) then
+              index=k
+              exit
+            endif
+          enddo !k
+          if(index==0) then
+            write(*,*)'Wrong ball info',i,j
+            stop
+          endif
+          elside(index,iel)=ns
+        endif !ic3(j,i).ne.0
+      endif !ic3(j,i)==0.or.i<ic3(j,i)
+    enddo !j
+  enddo !i=1,ne
+
+! Mark all bnd nodes
+  icolor=0
+  ibnd=0
+  do ie=1,ne
+    do j=1,3 !visit each side associated with element ie
+      nd1=elnode(nx(i34(ie),j,1),ie)
+      nd2=elnode(nx(i34(ie),j,2),ie)
+
+      if(ic3(j,ie)==0) then
+        ibnd(nd1)=1; ibnd(nd2)=1
+        !Find bnd near North pole
+        ytmp=maxval(ynd(elnode(:,ie)))
+        if(ytmp>84) then
+          write(*,*)'North pole is outside grid:',nd1,nd2
+          stop 
+        endif
+
+        !Antarctica
+        ytmp=minval(ynd(elnode(:,ie)))
+        if(ytmp<-63.06) then
+          icolor(nd1)=1
+          icolor(nd2)=1
+        endif
+      endif !ic3(j,ie)==0
+      
+    enddo !j
+  enddo !ie
+
+! Sort Antarctica bnd
+  open(13,file='bnd.tmp',status='replace')
+  if(icolor(node_ant)/=1) stop 'Input node is not on southern bnd'
+  next=node_ant
+  write(13,*)next,0,0 !last 2 values not important (for re-reading only)
+  icolor2=0
+  icolor2(next)=1 !in the list
+  nlnd=1
+  loop2: do 
+    next0=next !save
+    next=-1 !flag
+    loop1: do j=1,nne(next0)
+      ie=indel(j,next0)
+      do m=1,3 !sides
+        isd=elside(m,ie)
+        if(isdel(2,isd)/=0) cycle
+        if(isidenode(1,isd)/=next0.and.isidenode(2,isd)/=next0) cycle
+
+        nd=isidenode(1,isd)+isidenode(2,isd)-next0
+        if(nd/=next0.and.icolor(nd)==1) then
+          if(nlnd>2.and.nd==node_ant) then !closed the loop (assuming at least 3 nodes)
+            exit loop2
+          else if(icolor2(nd)==0) then !not in list; new node
+            next=nd
+            last_node=nd !save last node
+            icolor2(nd)=1 !put in the list
+            write(13,*)next,0,0
+            nlnd=nlnd+1
+            exit loop1
+          endif !nd
+        endif
+      enddo !m
+    enddo loop1 !j
+    if(next<0) then
+      write(*,*)'Failed to find next node:',i,next0
+      stop
+    endif
+ 
+  enddo loop2
+  
+  print*, nlnd,' land bnd nodes found in the south'
+
+  write(13,*)-1,nlnd,0 !'-1' as a flag for re-reading
+  !Close the bnd
+  write(13,*)last_node,0,0
+  write(13,*)node_ant,0,0
+  write(13,*)-1,2,0
+  nlnd=nlnd+2
+
+! Deal with islands
+  islands=0 !# of islands
+  do i=1,np
+    if(ibnd(i)==0.or.icolor2(i)==1) cycle
+
+    !bnd node
+    write(13,*)i,0,0
+    i00=i !save
+    next=i
+    icolor2(next)=1 !in the list
+    nlnd=nlnd+1
+    nlnd2=1 !# of nodes on this island
+    islands=islands+1
+    print*, 'starting node of new island:',i00
+    loop4: do 
+      next0=next !save
+      next=-1 !flag
+      loop3: do j=1,nne(next0)
+        ie=indel(j,next0)
+        do m=1,3 !side
+          isd=elside(m,ie)
+          if(isdel(2,isd)/=0) cycle
+          if(isidenode(1,isd)/=next0.and.isidenode(2,isd)/=next0) cycle
+
+          nd=isidenode(1,isd)+isidenode(2,isd)-next0
+          if(nd/=next0.and.ibnd(nd)==1) then
+            if(nlnd2>2.and.nd==i00) then !closed the loop (assuming at least 3 nodes)
+              exit loop4
+            else if(icolor2(nd)==0) then !not in list; new node
+              next=nd
+              icolor2(nd)=1 !put in the list
+              write(13,*)next,0,0
+              nlnd2=nlnd2+1
+              nlnd=nlnd+1
+              exit loop3
+            endif !nd
+          endif
+        enddo !m
+      enddo loop3 !j
+      if(next<0) then
+        write(*,*)'Failed to find next node:',i,next0
+        stop
+      endif
+    enddo loop4
+    write(13,*)-1,nlnd2,1
+    print*, nlnd2,' nodes on island # ',islands
+  enddo !i=1,np
+  print*, 'Total # of land nodes=',nlnd,sum(ibnd) !2 nodes repeated on outer bnd
+
+  close(13)
+  open(13,file='bnd.tmp',status='old')
+  open(10,file='bnd.out',status='replace')
+  write(10,*)0, '=open'; write(10,*)0;
+  write(10,*)2+islands !number of land boundaries
+  write(10,*)nlnd !Total number of open boundary nodes
+  nlines=0
+  indx=0 !array index
+  ilnd=-99 !flag
+  do 
+    read(13,*,end=100,err=100)i1,i2,i3
+    nlines=nlines+1
+    if(i1<0) then !reset index and output this seg
+      write(10,*)i2,i3
+      do i=1,indx
+        write(10,*)ilnd(i)
+      enddo !i
+      ilnd=-99 !flag
+      indx=0
+    else
+      indx=indx+1
+      ilnd(indx)=i1
+    endif
+  enddo
+100 print*, nlines,'  lines read from bnd output'
+  
+
+  stop
+  end
+
+      function signa(x1,x2,x3,y1,y2,y3)
+!...  Compute signed area formed by pts 1,2,3
+      implicit real*8(a-h,o-z)
+
+      signa=((x1-x3)*(y2-y3)-(x2-x3)*(y1-y3))/2
+
+      return
+      end
+
