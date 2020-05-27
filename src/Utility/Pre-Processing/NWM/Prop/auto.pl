9383720fdd6456e6d4a3c57d600e0095b4e9b5a7
Wei Huang <whuang07@hurricane.sciclone.wm.edu>
2020-05-27 12:05:12 -0400

    Updated auto.pl, added three rivers in Galveston Bay

--- src/Utility/Pre-Processing/NWM/Prop/auto.pl
+++ src/Utility/Pre-Processing/NWM/Prop/auto.pl
@@ -19,42 +19,26 @@ unlink("../tvd.prop");
 copy("tvd.prop","../tvd.prop");
 
 #set fluxflag.prop
-system("$script_dir/auto_edit_prop fluxflag/0.reg hgrid.ll 0 -1");
-move("out.prop","default.prop");
-system("$script_dir/auto_edit_prop fluxflag/1-.reg hgrid.ll 1 -9999");
-move("out.prop","default.prop");
-system("$script_dir/auto_edit_prop fluxflag/0.reg hgrid.ll 0 -9999");
-move("out.prop","default.prop");
-system("$script_dir/auto_edit_prop fluxflag/1+.reg hgrid.ll 1 -9999");
-move("out.prop","default.prop");
-system("$script_dir/auto_edit_prop fluxflag/2-.reg hgrid.ll 2 -9999");
-move("out.prop","default.prop");
-system("$script_dir/auto_edit_prop fluxflag/2+.reg hgrid.ll 2 -9999");
-move("out.prop","default.prop");
-system("$script_dir/auto_edit_prop fluxflag/3-.reg hgrid.ll 3 -9999");
-move("out.prop","default.prop");
-system("$script_dir/auto_edit_prop fluxflag/3+.reg hgrid.ll 3 -9999");
-move("out.prop","default.prop");
-system("$script_dir/auto_edit_prop fluxflag/4-.reg hgrid.ll 4 -9999");
-move("out.prop","default.prop");
-system("$script_dir/auto_edit_prop fluxflag/4+.reg hgrid.ll 4 -9999");
-move("out.prop","default.prop");
-system("$script_dir/auto_edit_prop fluxflag/5-.reg hgrid.ll 5 -9999");
-move("out.prop","default.prop");
-system("$script_dir/auto_edit_prop fluxflag/5+.reg hgrid.ll 5 -9999");
-move("out.prop","default.prop");
-system("$script_dir/auto_edit_prop fluxflag/6-.reg hgrid.ll 6 -9999");
-move("out.prop","default.prop");
-system("$script_dir/auto_edit_prop fluxflag/6+.reg hgrid.ll 6 -9999");
-move("out.prop","default.prop");
-system("$script_dir/auto_edit_prop fluxflag/7-.reg hgrid.ll 7 -9999");
-move("out.prop","default.prop");
-system("$script_dir/auto_edit_prop fluxflag/7+.reg hgrid.ll 7 -9999");
-move("out.prop","default.prop");
-system("$script_dir/auto_edit_prop fluxflag/8.reg hgrid.ll 8 -9999");
-move("out.prop","fluxflag.prop");
 
-unlink("../fluxflag.prop");
+system("$script_dir/auto_edit_prop fluxflag/0-.reg hgrid.ll 0 -1");
+move("out.prop","default.prop");
+system("$script_dir/auto_edit_prop fluxflag/0+.reg hgrid.ll 1 -9999");
+move("out.prop","default.prop");
+
+for (my $i=1; $i <= 2; $i++) {
+    $region="$i-.reg";
+    system("$script_dir/auto_edit_prop fluxflag/$region hgrid.ll $i -9999");
+    move("out.prop","default.prop");
+    $region="$i+.reg";
+    $i1=$i+1;
+    system("$script_dir/auto_edit_prop fluxflag/$region hgrid.ll $i1 -9999");
+    move("out.prop","default.prop");
+}
+
+move("default.prop","fluxflag.prop");
+
 copy("fluxflag.prop","../fluxflag.prop");
 
 print("Done.\n")
+
+
