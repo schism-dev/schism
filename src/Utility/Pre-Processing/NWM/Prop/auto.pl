18b2648a4ff7d3d720a6714a8b3dd5d35fd6789f
Fei Ye <feiye@vims.edu>
2020-05-28 23:53:55 -0400

    NWM: updated the Prop script.

--- src/Utility/Pre-Processing/NWM/Prop/auto.pl
+++ src/Utility/Pre-Processing/NWM/Prop/auto.pl
@@ -12,33 +12,42 @@ $script_dir="../Grid_manipulation/";
 #UTM grid
 system("ln -sf ../hgrid.* .");
 
+#-----------------------fluxflag-------------------
+@reg_list = glob("fluxflag/*+.reg");
+$dummy_reg = $reg_list[0];
+#initialize background as -1
+system("$script_dir/auto_edit_prop $dummy_reg hgrid.ll -1 -1");
+move("out.prop","default.prop");
+
+my $n = 0;
+foreach (@reg_list) {
+    $upstream_reg = $_;
+    $downstream_reg = $upstream_reg;
+    $downstream_reg =~ s/\+/\-/g;
+    print("$upstream_reg $downstream_reg\n");
+
+    print("setting $downstream_reg to $n and $upstream_reg to $m\n");
+    print("$script_dir/auto_edit_prop $downstream_reg hgrid.ll $n -9999\n");
+    system("$script_dir/auto_edit_prop $downstream_reg hgrid.ll $n -9999");
+    move("out.prop","default.prop");
+
+    $n = $n+1;
+    print("$script_dir/auto_edit_prop $upstream_reg hgrid.ll $n -9999\n");
+    system("$script_dir/auto_edit_prop $upstream_reg hgrid.ll $n -9999");
+    move("out.prop","default.prop");
+}
+move("default.prop","fluxflag.prop");
+
+unlink("../fluxflag.prop");
+copy("fluxflag.prop","../fluxflag.prop");
+
+
+#-----------------------tvd-------------------
 #set tvd.prop
 system("$script_dir/auto_edit_prop tvd.rgn hgrid.ll 0 1");
 move("out.prop","tvd.prop");
 unlink("../tvd.prop");
 copy("tvd.prop","../tvd.prop");
 
-#set fluxflag.prop
-
-system("$script_dir/auto_edit_prop fluxflag/0-.reg hgrid.ll 0 -1");
-move("out.prop","default.prop");
-system("$script_dir/auto_edit_prop fluxflag/0+.reg hgrid.ll 1 -9999");
-move("out.prop","default.prop");
-
-for (my $i=1; $i <= 2; $i++) {
-    $region="$i-.reg";
-    system("$script_dir/auto_edit_prop fluxflag/$region hgrid.ll $i -9999");
-    move("out.prop","default.prop");
-    $region="$i+.reg";
-    $i1=$i+1;
-    system("$script_dir/auto_edit_prop fluxflag/$region hgrid.ll $i1 -9999");
-    move("out.prop","default.prop");
-}
-
-move("default.prop","fluxflag.prop");
-
-copy("fluxflag.prop","../fluxflag.prop");
 
 print("Done.\n")
-
-
