681377d86b930d223a58eaad4dca8e1cdd50bf46
Fei Ye <feiye@vims.edu>
2020-07-08 19:27:11 -0400

    Change the criterion of turning off air-sea exchange from grid depth to water depth.

--- src/Hydro/schism_step.F90
+++ src/Hydro/schism_step.F90
@@ -6151,7 +6151,7 @@
         if(isconsv/=0) then
 !$OMP     do 
           do i=1,nea
-            if(idry_e(i)==1.or.dpe(i)<hmin_airsea_ex) cycle
+            if( idry_e(i)==1 .or. (ze(nvrt,i)-ze(kbe(i),i))<hmin_airsea_ex ) cycle
 
             if(impose_net_flux/=0) then !imposed net 
               precip=sum(fluxprc(elnode(1:i34(i),i)))/real(i34(i),rkind) !P-E
@@ -6169,7 +6169,7 @@
         if(ihconsv/=0) then
 !$OMP     do 
           do i=1,nea
-            if(idry_e(i)==1.or.dpe(i)<hmin_airsea_ex) cycle
+            if( idry_e(i)==1 .or. (ze(nvrt,i)-ze(kbe(i),i))<hmin_airsea_ex ) cycle
 
 !           Wet element (not shallow)
 !           Surface flux
