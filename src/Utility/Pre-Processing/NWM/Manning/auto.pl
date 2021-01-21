366418a8b8f6bf4ea81af975569769b3f3b02b5b
Fei Ye <feiye@vims.edu>
2021-01-20 21:57:55 -0500

    NWM scripts: add GoME regions for setting Manning's n locally. Other minor changes.

--- src/Utility/Pre-Processing/NWM/Manning/auto.pl
+++ src/Utility/Pre-Processing/NWM/Manning/auto.pl
@@ -14,6 +14,7 @@ print("e.g: ifort -O2 -mcmodel=medium -CB -Bstatic -o gen_bottom_fric gen_bottom
 #$rundir = cwd();
 $script_dir="../Grid_manipulation/";
 
+#hgrid.gr3 is in lon/lat
 system("ln -sf ../hgrid.gr3 .");
 system("ln -sf ../vgrid.in .");
 
@@ -22,10 +23,20 @@ system("ln -sf ../vgrid.in .");
 #move("out.gr3","include.gr3");
 #
 unless (-e "bottom_fric.in") {
-  print ("'bottom_fric.in' not found in the current dir, see examples in Sample_*/\n");
+  print ("'bottom_fric.in' not found in the current dir, see examples in ./Sample_bottom_fric/\n");
   exit;
 }
 system("./gen_bottom_fric < bottom_fric.in");
 
+
+#set special values in GoME regions
+system("$script_dir/auto_edit_region 1 GoME_1.reg bottom_friction.gr3 0.05");
+move("out.gr3","bottom_friction.gr3");
+system("$script_dir/auto_edit_region 1 GoME_2.reg bottom_friction.gr3 0.05");
+move("out.gr3","bottom_friction.gr3");
+
+
 unlink("../manning.gr3");
 copy("bottom_friction.gr3","../manning.gr3");
+
+
