96263cbdcc1ac27fb22a0a3762e23ee7ab6ab07e
Joseph Zhang <yjzhang@vims.edu>
2021-04-20 13:34:09 -0400

    work on periodic b.c. grid

--- src/Utility/Grid_Scripts/periodic_grid.f90
+++ src/Utility/Grid_Scripts/periodic_grid.f90
@@ -5,7 +5,7 @@
 !     The grid actually has no boundary at dateline (\pm 180
 !     degrees), so no b.c. is needed there (i.e. a periodic construction).
 !     The script basically drapes the original Cartesian grid onto a
-!     sphere, with lat=[-0.5,0.5] in order to have minimal distortion to
+!     sphere, with lat centered around equator in order to have minimal distortion to
 !     the distance along the meridional
 
 !     Inputs: constants below
@@ -19,31 +19,39 @@
 
       pi=acos(-1.d0)
       !Lengths in x,y (m)
-      rlx=100e3; rly=10e3
+      rlx=1100e3 
+      rly=500e3
       !Resolution in x,y (m)
       !# of divisions in x must >=360
-      dx=rlx/361; dy=1e3
+      nx=1100 !# of divisions in x (including wrap-around seg)
+      if(nx<360) stop 'Make sure # of divisions in x >=360!'
+      dx=rlx/nx 
+      dy=dx
+      ny=rly/dy+0.01 !# of divisions in y
 
-      !Min/max lat (degr) - use a small meridional band to minimize
-      !distorion
       !The global (x,y,z) are calculated via:
       !x=R_e*cos(\phi)*cos(\lambda)
       !y=R_e*cos(\phi)*sin(\lambda)
       !z=R_p*sin(\phi)
-      ymin=-1; ymax=1
 
       !Radius @ pole to match rly
-      r_pole=rly/(ymax-ymin)/pi*180
-      
-      nx=rlx/dx+0.01 !# of divisions in x
-      ny=rly/dy+0.01 !# of divisions in y
-      if(nx<360) stop 'Make sure # of divisions in x >=360!'
+      !r_pole=rly/(ymax-ymin)/pi*180
 
       !Back calculate radius of 'earth' @ equator to match length in x
+      !Experience suggests that the 2 radii should be same (otherwise
+      !code may crash)
       r_eq=rlx/2/pi 
+      r_pole=r_eq !*1.2
+
+      !Min/max lat (degr) - use a small meridional band to minimize
+      !distorion
+      y_total=rly/pi/r_pole*180
+      ymax=y_total/2; ymin=-ymax
+      print*, 'ymax|min (deg)=',ymin,ymax
+      if(ymax>=90.or.ymin<=-90) stop 'lat wrong'
 
       !Resolutions in lon/lat (degr)
-      dx_lon=360./(nx+1)
+      dx_lon=360./nx !(nx+1)
       dy_lat=(ymax-ymin)/ny
       !Min/max lon (degr)
       xmin=-180
@@ -51,7 +59,7 @@
 
       print*, 'Radii @ pole and equator=',r_pole,r_eq
       print*, 'nx,ny=',nx,ny
-      print*, 'Resolutions in meter=',dx,dy
+      print*, 'Target resolutions in meter=',dx,dy
       print*, 'Resolutions in lon/lat (degr)=',dx_lon,dy_lat
 
       open(12,file='hgrid.ll',status='replace')
@@ -60,16 +68,16 @@
         stop
       endif
 
-      do ix=1,nx+1
+      do ix=1,nx !+1
       do iy=1,ny+1
         nd=(ny+1)*(ix-1)+iy
-        x(nd)=xmin+(xmax-xmin)/nx*(ix-1)
+        x(nd)=xmin+(xmax-xmin)/(nx-1)*(ix-1)
         y(nd)=ymin+(ymax-ymin)/ny*(iy-1)
       enddo
       enddo
-      nn=(nx+1)*(ny+1)
+      nn=nx*(ny+1)
 
-      do ix=1,nx
+      do ix=1,nx-1
       do iy=1,ny
          !Split into pairs of tri's
          ne=ny*(ix-1)+iy
@@ -83,7 +91,7 @@
          ic1(2*ne,3)=nd1+1
       enddo
       enddo
-      nt=nx*ny*2
+      nt=(nx-1)*ny*2
       nt0=nt
 
 !     Add wrap-around elements
@@ -106,7 +114,7 @@
       write(9,*)
       write(9,*)nt,nn
       do i=1,nn
-        write(12,100)i,x(i),y(i),50.
+        write(12,100)i,x(i),y(i),1.e3
 
         !global coord. (SCHISM)
         x3(i)=r_eq*cos(y(i)/180*pi)*cos(x(i)/180*pi)
@@ -137,9 +145,9 @@
       write(12,*)0,' = open' !# of open bnd
       write(12,*)0 !total # of open bnd  nodes
       write(12,*)4 !# of land bnd
-      write(12,*)(nx+3)*2 !total # of land bnd  nodes
-      write(12,*)nx+1,0 !1st land bnd
-      do i=1,nx+1
+      write(12,*)(nx+2)*2 !total # of land bnd  nodes
+      write(12,*)nx,0 !1st land bnd
+      do i=1,nx
         write(12,*)1+(i-1)*(ny+1)
       enddo !i
       write(12,*)2,0 !2nd land bnd, across dateline
@@ -148,8 +156,8 @@
       write(12,*)2,0 !3rd land bnd
       write(12,*)ny+1
       write(12,*)nn
-      write(12,*)nx+1,0 !4th bnd
-      do i=1,nx+1
+      write(12,*)nx,0 !4th bnd
+      do i=1,nx
         write(12,*)nn-(i-1)*(ny+1)
       enddo !i
 
