11be79253f838766ced6649ced9ddfbfa44ca132
Joseph Zhang <yjzhang@vims.edu>
2023-09-12 08:47:46 -0400

    Remove a redundant script

--- src/Utility/Pre-Processing/gen_source.f90
+++ src/Utility/Pre-Processing/gen_source.f90
@@ -1,7 +1,8 @@
-!     Given a list of pts, find parent elements in hgrid.gr3 (tri or quad), and generate source_sink.in etc 
+!     Example script that given a list of pts, finds parent elements in hgrid.gr3 (tri or quad), and generate source_sink.in etc 
 !     Search for parent elements along x- or y- strips (use small mne_bin for efficiency)
 !     Works for mixed tri/quad
-!     Inputs: ss_pt.bp (list of pts, with z related to volume rate (positive for sinks); see line 217 onwards)
+!     Inputs: ss_pt.bp (list of pts, with z related to volume rate (positive for sinks). In this
+!             example, it's rain rate. See line 217 onwards)
 !             hgrid.gr3 
 !             gen_source.in (see sample below): 
 !                                         (1) is_xy: search along x (1) or y (2);
@@ -217,7 +218,7 @@
       total_r=0
       ifl_el=0 !flag for each bg elem.
       do i=1,npfg
-        read(12,*)j,xfg,yfg,vrate(i) !vrate: m/year (positive ->sink)
+        read(12,*)j,xfg,yfg,vrate(i) !vrate: m/year (positive ->sink) for rainfall rate
         if(vrate(i)==0) cycle
 
         call binsearch(i,xfg,yfg,iparen(i),arco)
