5cc2da7202aa9afe570dc10510962f58631cc9dd
cuill <cui86ll@gmail.com>
2024-01-02 11:29:19 -0500

    Update scripts for STOFS-3D Alt/Pac

--- src/Utility/Pre-Processing/STOFS-3D-Pac-shadow-VIMS/River/gen_TEM_1.py
+++ src/Utility/Pre-Processing/STOFS-3D-Pac-shadow-VIMS/River/gen_TEM_1.py
@@ -1,79 +1,101 @@
-import argparse
 from datetime import datetime, timedelta
-import glob
-import os
 
 import numpy as np
 import pandas as pd
 
-def get_river_obs(df, datevectors, datevectors2):
-    #df = pd.read_csv(fname, sep=',', na_values='')
-    df.drop(df.columns[[1, 3, 4, 5]],axis=1, inplace=True)
-    #df.rename(columns={df.columns[0]: 'date_local', df.columns[1]: 'flow'}, inplace=True)
-    ts = pd.to_datetime(pd.Series(df['date'].values))
-    ts2 = ts.dt.tz_localize('US/Eastern', ambiguous='infer', nonexistent='shift_forward')
+unit_conv = 0.028316846
+
+def get_usgs_obs(fname=None, datevectors=None):
+    df = pd.read_csv(fname, skiprows=26,sep='\t', na_values=' ')
+    df.drop([0], inplace=True)
+    tz = df['tz_cd'].iloc[1]
+    #df.drop(columns=['agency_cd', 'site_no', 'tz_cd', '117345_00060_cd'], inplace=True)
+    df.drop(df.columns[[0, 1, 3, 5]], axis=1, inplace=True)
+    ts = pd.to_datetime(pd.Series(df['datetime'].values))
+    ts2 = ts.dt.tz_localize('US/Pacific', ambiguous='infer', nonexistent='shift_forward')
+    ts3 = ts2.dt.tz_convert('UTC')
+    df.drop(columns=['datetime'], inplace=True)
+    df.reset_index(drop=True, inplace=True)
+    df.insert(1, 'date_utc', ts3)
+    df.set_index('date_utc', inplace=True)
+    df.rename(columns={df.columns[0]: 'temp'}, inplace=True)
+
+    data = []
+    for dt in datevectors:
+        data.append(float(df.loc[dt]['temp']))
+    data.append(data[-1])
+    data.append(data[-1])
+    return data
+
+def get_columbia_river_obs(fname, datevectors):
+    df = pd.read_csv(fname, sep=',', na_values=' ')
+    df= df.rename(columns={df.columns[1]: 'T-F'})
+    #convert PST to UTC (be careful with DST)
+    ts = pd.to_datetime(pd.Series(df['timestamp'].values))
+    ts2 = ts.dt.tz_localize('US/Pacific', ambiguous='infer', nonexistent='shift_forward')
     ts3 = ts2.dt.tz_convert('UTC')
     df.insert(1, 'date_utc', ts3)
-    #df.drop('date', inplace=True)
-    df.set_index('date_utc', inplace=True)
-    df_daily = df.resample('D', closed='left').mean()
-    #check missing values
-    if df_daily['t'].isnull().sum() > 0:
-        df_daily['t'] = df_daily['t'].interpolate()
-    
-    data = []
-    for i, dt in enumerate(datevectors):
-        data.append(float(df_daily.loc[dt].values[0]))
 
-    for dt in datevectors2[i+1:]:
-        data.append(data[-1])
+    df.set_index('date_utc', inplace=True)
+    df.drop(columns=['timestamp'], inplace=True)
+    data = []
+    for dt in datevectors:
+        data.append(round((float(df.loc[dt]['T-F']) - 32) * 5 / 9, 1))
+    data.append(data[-1])
+    data.append(data[-1])
+    return data
+
+def get_fraser_river_obs(fname, datevectors):
+    df = pd.read_csv(fname, sep=',', na_values='')
+    #df.drop(df.columns[[0, 2, 3, 4, 5, 7, 8, 9]],axis=1, inplace=True)
+    #df.rename(columns={df.columns[0]: 'date_local', df.columns[1]: 'flow'}, inplace=True)
+    df['date_utc']  = pd.to_datetime(pd.Series(df['date_utc'].values))
+    #ts2 = ts.dt.tz_localize('US/Pacific', ambiguous='infer', nonexistent='shift_forward')
+    #ts3 = ts.dt.tz_convert('UTC')
+    #df.insert(1, 'date_utc', ts3)
+    #df.drop('date_local', inplace=True)
+    df.set_index('date_utc', inplace=True)
+    data = []
+    for dt in datevectors:
+        data.append(float(df.loc[dt].values[0]))
+    data.append(data[-1])
     data.append(data[-1])
     return data
 
 if __name__ == '__main__':
-    '''
-    python gen_TEM_1_rescue.py startdate enddate
- 
-    Assume you have all QC_waterT_*.csv files in the current folder.
-    The "enddate" should be the second date in the latest filename. For example:
-    the latest filename is "QC_waterT_2023-07-19_2023-07-26.csv", then enddate should be 2023-07-26. The
-    script will extend 5 days to the further from enddate.
-    python gen_TEM_1_rescue.py 2023-07-01 2023-07-26
-    '''
-
-    #input paramters 
-    argparser = argparse.ArgumentParser()
-    argparser.add_argument('startdate', type=datetime.fromisoformat, help='input startdate')
-    argparser.add_argument('enddate', type=datetime.fromisoformat, help='input enddate')
-    args=argparser.parse_args()
-    startDT=args.startdate
-    endDT=args.enddate
-
+    date = datetime.now() - timedelta(days=1)
+    startDT = datetime(date.year, date.month, date.day)
+    endDT = startDT + timedelta(days=1)
     datevectors = pd.date_range(start=startDT.strftime('%Y-%m-%d'), end=endDT.strftime('%Y-%m-%d'))
-    #datevector2 - real time in TEM_1.th
-    endDT2 = endDT + timedelta(days=5)
+    endDT2 = startDT + timedelta(days=3)
     datevectors2 = pd.date_range(start=startDT.strftime('%Y-%m-%d'), end=endDT2.strftime('%Y-%m-%d'))
 
-    #combine csv files
-    files = glob.glob('QC_waterT_*.csv')
-    files.sort()
+    #orders[Willamette, columbia/3, columbia/3, columbia/3, lewis, Cowlitz, fraser]
+    #No observation at Levis and Cowlitz, use Columbia data
+    rivers = ['Willamette', 'Columbia', 'Columbia', 'Columbia', 'Columbia', 'Columbia', 'Fraser']
 
-    #check date
-    date0 = files[0].split('_')[2]
-    date1 = files[-1].split('_')[3].split('.')[0]
-    if startDT < datetime.strptime(date0, '%Y-%m-%d'):
-        raise ValueError(f'startdate {startDT} is ahead of date {date0} in available files!')
-    if endDT > datetime.strptime(date1, '%Y-%m-%d'):
-        raise ValueError(f'enddate {endDT} exceeds date {date1} in available files!')
-    
-    df = pd.concat(map(pd.read_csv, files), ignore_index=True)
-    df.drop_duplicates(subset='date', keep='last', inplace=True, ignore_index=True)
+    #station ids
+    station_ids = {'Willamette': '14211720'}
+    fname_fraser = 'FraserRiver_real-time_waterT.csv'
+    fname_columbia_BON = 'BON.Temp-Water.Inst.1Hour.txt'
+    fname_columbia_WRNO = 'WRNO.Temp-Water.Inst.1Hour.txt'
 
     temp = {}
-    #get st lawrence river
-    temp['SL'] = get_river_obs(df, datevectors, datevectors2)
+    for key, sid in station_ids.items():
+        fname = f'usgs_temp_{sid}.csv'
+        temp[key] = get_usgs_obs(fname, datevectors)
 
-    rivers = ['SL']
+    #get fraser river
+    temp['Fraser'] = get_fraser_river_obs(fname_fraser, datevectors)
+
+    #get columbia river
+    try:
+        print(f'Get temperature data  from Bonneville')
+        temp['Columbia'] = get_columbia_river_obs(fname_columbia_BON, datevectors)
+    except:
+        print(f'No temperature data  at Bonneville, try Warrendale')
+        temp['Columbia'] = get_columbia_river_obs(fname_columbia_WRNO, datevectors)
+
     #write file
     data = []
     for i, date in enumerate(datevectors2):
@@ -88,3 +110,5 @@ if __name__ == '__main__':
 
     newset = np.array(data)
     np.savetxt('TEM_1.th', newset, fmt='%.2f') 
+       
+    
--- src/Utility/Pre-Processing/STOFS-3D-Pac-shadow-VIMS/River/gen_fluxth.py
+++ src/Utility/Pre-Processing/STOFS-3D-Pac-shadow-VIMS/River/gen_fluxth.py
@@ -1,13 +1,52 @@
-import argparse
 from datetime import datetime, timedelta
-import os
-import glob
 
 import numpy as np
 import pandas as pd
 
-def get_river_obs(df, datevectors, datevectors2):
-    #df = pd.read_csv(fname, sep=',', na_values='')
+unit_conv = 0.028316846
+
+def get_usgs_obs(fname=None, datevectors=None):
+    df = pd.read_csv(fname, skiprows=26,sep='\t', na_values=' ')
+    df.drop([0], inplace=True)
+    tz = df['tz_cd'].iloc[1]
+    #df.drop(columns=['agency_cd', 'site_no', 'tz_cd', '117345_00060_cd'], inplace=True)
+    df.drop(df.columns[[0, 1, 3, 5]], axis=1, inplace=True)
+    ts = pd.to_datetime(pd.Series(df['datetime'].values))
+    ts2 = ts.dt.tz_localize('US/Pacific', ambiguous='infer', nonexistent='shift_forward')
+    ts3 = ts2.dt.tz_convert('UTC')
+    df.drop(columns=['datetime'], inplace=True)
+    df.reset_index(drop=True, inplace=True)
+    df.insert(1, 'date_utc', ts3)
+    df.set_index('date_utc', inplace=True)
+    df.rename(columns={df.columns[0]: 'flow'}, inplace=True)
+
+    data = []
+    for dt in datevectors:
+        data.append(round(float(df.loc[dt]['flow'])*unit_conv, 3))
+    data.append(data[-1])
+    data.append(data[-1])
+    return data
+
+def get_columbia_river_obs(fname, datevectors):
+    df = pd.read_csv(fname, sep=',', na_values=' ')
+    df= df.rename(columns={df.columns[1]: 'flow'})
+    #convert PST to UTC (be careful with DST)
+    ts = pd.to_datetime(pd.Series(df['timestamp'].values))
+    ts2 = ts.dt.tz_localize('US/Pacific', ambiguous='infer', nonexistent='shift_forward')
+    ts3 = ts2.dt.tz_convert('UTC')
+    df.insert(1, 'date_utc', ts3)
+
+    df.set_index('date_utc', inplace=True)
+    df.drop(columns=['timestamp'], inplace=True)
+    data = []
+    for dt in datevectors:
+        data.append(round(float(df.loc[dt]['flow'])*1000*unit_conv, 3))
+    data.append(data[-1])
+    data.append(data[-1])
+    return data
+
+def get_fraser_river_obs(fname, datevectors):
+    df = pd.read_csv(fname, sep=',', na_values='')
     df.drop(df.columns[[0, 2, 3, 4, 5, 7, 8, 9]],axis=1, inplace=True)
     df.rename(columns={df.columns[0]: 'date_local', df.columns[1]: 'flow'}, inplace=True)
     ts = pd.to_datetime(pd.Series(df['date_local'].values))
@@ -16,67 +55,42 @@ def get_river_obs(df, datevectors, datevectors2):
     df.insert(1, 'date_utc', ts3)
     #df.drop('date_local', inplace=True)
     df.set_index('date_utc', inplace=True)
-
-    df_daily = df.resample('D', closed='left').mean()
-    #check missing values
-    if df_daily['flow'].isnull().sum() > 0:
-        df_daily['flow'] = df_daily['flow'].interpolate()
-
     data = []
-    for i, dt in enumerate(datevectors):
-        data.append(round(float(df_daily.loc[dt]['flow']), 3))
-
-    for dt in datevectors2[i+1:]:
-        data.append(data[-1])
+    breakpoint()
+    for dt in datevectors:
+        data.append(round(float(df.loc[dt]['flow']), 3))
+    data.append(data[-1])
+    data.append(data[-1])
     return data
 
 if __name__ == '__main__':
-
-    '''
-    python gen_fluxth.py startdate enddate
- 
-    Assume you have all St_Lawrence_river_discharge_*.csv files in the current folder.
-    The "enddate" should be the date in the latest filename. For example:
-    the latest filename is "St_Lawrence_river_discharge_2023-07-26.csv", 
-    then enddate should be 2023-07-26. The
-    script will extend 5 days to the further from enddate.
-    python gen_fluxth.py 2023-07-01 2023-07-26
-    '''
-    #input paramters 
-    argparser = argparse.ArgumentParser()
-    argparser.add_argument('startdate', type=datetime.fromisoformat, help='input startdate')
-    argparser.add_argument('enddate', type=datetime.fromisoformat, help='input enddate')
-    args=argparser.parse_args()
-    startDT=args.startdate
-    endDT=args.enddate
-
+    date = datetime.now() - timedelta(days=1)
+    startDT = datetime(date.year, date.month, date.day)
+    endDT = startDT + timedelta(days=1)
     datevectors = pd.date_range(start=startDT.strftime('%Y-%m-%d'), end=endDT.strftime('%Y-%m-%d'))
-    #datevector2 - real time in TEM_1.th
-    endDT2 = endDT + timedelta(days=5)
+    endDT2 = startDT + timedelta(days=3)
     datevectors2 = pd.date_range(start=startDT.strftime('%Y-%m-%d'), end=endDT2.strftime('%Y-%m-%d'))
 
-    #combine csv files
-    files = glob.glob('St_Lawrence_river_discharge_*.csv')
-    files.sort()
+    #orders[Willamette, columbia/3, columbia/3, columbia/3, lewis, Cowlitz, fraser]
+    rivers = ['Willamette', 'Columbia', 'Columbia', 'Columbia', 'Lewis', 'Cowlitz', 'Fraser']
 
-    #check date
-    date0 = files[0].split('_')[-1].split('.')[0]
-    date1 = datetime.strptime(date0, '%Y-%m-%d') - timedelta(days=3)
-    date2 = files[-1].split('_')[-1].split('.')[0]
-    if startDT < date1:
-        raise ValueError(f'startdate {startDT} is ahead of date {date1} in available files!')
-    if endDT > datetime.strptime(date2, '%Y-%m-%d'):
-        raise ValueError(f'enddate {endDT} exceeds date {date2} in available files!')
-    
-    df = pd.concat(map(pd.read_csv, files), ignore_index=True)
-    df.drop_duplicates(subset='Date', keep='last', inplace=True, ignore_index=True)
+    #station ids
+    station_ids = {'Willamette': '14211720', 'Lewis': '14220500', 'Cowlitz': '14243000'}
+    fname_fraser = 'fraser_river_08MF005_hourly.csv'
+    fname_columbia = 'BON.Flow-Out.Ave.1Hour.txt'
 
     flow = {}
-    #get st Lawrence river
-    flow['SL'] = get_river_obs(df, datevectors, datevectors2)
+    for key, sid in station_ids.items():
+        fname = f'usgs_flow_{sid}.csv'
+        flow[key] = get_usgs_obs(fname, datevectors)
+
+    #get fraser river
+    flow['Fraser'] = get_fraser_river_obs(fname_fraser, datevectors)
+
+    #get columbia river
+    flow['Columbia'] = get_columbia_river_obs(fname_columbia, datevectors)
 
 
-    rivers = ['SL']
     #write file
     data = []
     for i, date in enumerate(datevectors2):
@@ -85,9 +99,14 @@ if __name__ == '__main__':
         print(f'time = {dt}')
         line.append(dt)
         for riv in rivers:
-            line.append(-flow[riv][i])
+            if riv == 'Columbia':
+                line.append(-round(flow[riv][i]/3, 3))
+            else:
+                line.append(-flow[riv][i])
 
         data.append(line)
 
     newset = np.array(data)
     np.savetxt('flux.th', newset, fmt='%.3f') 
+       
+    
