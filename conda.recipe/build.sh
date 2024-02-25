339d9db052108d5e20188bf5eb71c2ea199578dd
George Breyiannis <gbreyiannis@gmail.com>
2024-02-25 14:36:20 +0100

    finalize conda recipe (#101)
    * finalize conda recipe
    
    * update conda recipe
    
    Include python as differentiator

--- conda.recipe/build.sh
+++ conda.recipe/build.sh
@@ -2,41 +2,54 @@
 
 set -xeuo pipefail
 
+#export mpi=mpich
 export CC=mpicc
 export CXX=mpicxx
 export FC=mpif90
 export F77=mpif77
 export F90=mpif90
 
+#Remove metis from cmake
+sed -i -e "s|set(PARMETIS_VER|#set(PARMETIS_VER|g" src/CMakeLists.txt
+sed -i -e "s|set(PARMETIS_DIR|#set(PARMETIS_DIR|g" src/CMakeLists.txt
+sed -i -e "s|add_subdirectory( \${PARMETIS_VER} )|#add_subdirectory( \${PARMETIS_VER} )|g" src/CMakeLists.txt
+
 # build and install schism
 mkdir build
 cd build
 
-# check if needed on Linux
-#CMAKE_PLATFORM_FLAGS+=(-DCMAKE_TOOLCHAIN_FILE="${RECIPE_DIR}/cross-linux.cmake")
-#   ${CMAKE_PLATFORM_FLAGS[@]} \
-
 cmake -DCMAKE_INSTALL_PREFIX=$PREFIX -DCMAKE_BUILD_TYPE="Release" \
     -DCMAKE_Fortran_FLAGS_RELEASE_INIT="-O2 -ffree-line-length-none" \
-    -DCMAKE_Fortran_FLAGS_DEBUG_INIT="-g -ffree-line-length-none" \
-    -DCMAKE_Fortran_FLAGS_RELWITHDEBINFO_INIT="-O2 -g -ffree-line-length-none" \
-    -DC_PREPROCESS_FLAG="-cpp" \
-    -DNetCDF_FORTRAN_DIR="$PREFIX/lib" \
-    -DNetCDF_C_DIR="$PREFIX/lib" \
-    -DNetCDF_LIBRARIES="$PREFIX/lib/libnetcdff.dylib;$PREFIX/lib/libnetcdf.dylib" \
-    -DNetCDF_INCLUDE_DIR="$PREFIX/include" \
-    -DTVD_LIM="VL" \
+    -DTVD_LIM=$TVD_LIM \
+    -DOLDIO=$OLDIO \
+    -DPREC_EVAP=$PREC_EVAP \
+    -DUSE_GOTM=$GOTM \
+    -DUSE_HA=$HA \
+    -DUSE_SED2D=$SED2D \
+    -DUSE_MARSH=$MARSH \
+    -DUSE_WWM=$WWM \
+    -DUSE_WW3=$WW3 \
+    -DUSE_ICE=$ICE \
+    -DUSE_ICM=$ICM \
+    -DUSE_GEN=$GEN \
+    -DUSE_AGE=$AGE \
+    -DUSE_ECO=$ECO \
+    -DICM_PH=$PH \
+    -DUSE_COSINE=$COSINE \
+    -DUSE_FIB=$FIB \
+    -DUSE_FABM=$FABM \
+    -DUSE_SED=$SED \
     ../src
-#make -j${CPU_COUNT:-1} # It doesn't seem to work because of the build of parmetis
+
 make
 
-#make install
+##make install
 cp -r bin/* $PREFIX/bin/
-#ln -s $PREFIX/bin/pschism_TVD-VL $PREFIX/bin/schism # optional to be discussed
+#make a symlink for convenience
+ln -s $PREFIX/bin/pschism_TVD-$TVD_LIM $PREFIX/bin/schism
 
 #clean up
 cd ..
 rm -r build
 
 cd $BUILD_PREFIX
-
