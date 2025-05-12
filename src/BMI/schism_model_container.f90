f2ff3b1c0c43ddd4c78b5a0c5fa5bbde87c00eaa
Phil Miller <philip.miller@noaa.gov>
2025-05-12 10:12:09 -0700

    Clean up some superfluous diffs

--- src/BMI/schism_model_container.f90
+++ src/BMI/schism_model_container.f90
@@ -6,7 +6,7 @@ module schism_model_container
       integer :: num_time_steps
       double precision :: current_model_time
       double precision :: time_step_size
-      
+
       character(len=1000) :: SCHISM_dir
       integer :: SCHISM_NSCRIBES
 
