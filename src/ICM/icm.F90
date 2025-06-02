2bfe3acd622345330aea0699e41a0078831adbd8
Zhengui Wang <wzhengui@gmail.com>
2025-06-02 16:26:06 -0400

    continue to fix Denitrification issue when KhDOox=0.0 & DOX=0.0

--- src/ICM/icm.F90
+++ src/ICM/icm.F90
@@ -263,7 +263,7 @@ subroutine ecosystem(it)
 
         !respiration, denitrification, decay of COD, nitrification
         xT=temp(k)-TNit
-        rKHR(k)=rKC(3,k)*DOX(k)/(KhDOox+DOX(k))
+        rKHR(k)=rKC(3,k)*DOX(k)/(KhDOox+DOX(k)+1.d-6)
         rKCOD(k)=(DOX(k)/(KhCOD+DOX(k)))*KCD*exp(KTRCOD*(temp(k)-TRCOD))
         rDenit(k)=an2c*rKC(3,k)*KhDOox*NO3(k)/(KhDOox+DOX(k)+1.d-6)/(KhNO3dn+NO3(k))
         !rNit(k)=(DOX(k)*Nit*KhNH4n/((KhNH4n+NH4(k))*(KhDOn+DOX(k))))*exp(-max(-KTNit(1)*xT,KTNit(2)*xT)*abs(xT))
