042b37527b08c37f3e7839e62a98d32fa1ca4210
Joseph Zhang <yjzhang@vims.edu>
2026-03-15 13:59:13 -0400

    Edit Joey Smith's PR: exchange tau_oi(2,npa)

--- src/Hydro/schism_init.F90
+++ src/Hydro/schism_init.F90
@@ -1695,29 +1695,29 @@
 #endif
 
 #ifdef USE_FIB
-       allocate(kk_fib(nea,2),sink_fib(nea),fraction_fib(nea))
-       allocate(sink0(npa),fraction0(npa),kk10(npa),kk20(npa))
+         allocate(kk_fib(nea,2),sink_fib(nea),fraction_fib(nea))
+         allocate(sink0(npa),fraction0(npa),kk10(npa),kk20(npa))
 #endif
 
 #ifdef USE_CICE
-      allocate(aice(npa),tau_oi(2,npa),fresh_wa_flux(npa),salinity_flux(npa),net_heat_flux(npa), &
+        allocate(aice(npa),tau_oi(2,npa),fresh_wa_flux(npa),salinity_flux(npa),net_heat_flux(npa), &
                srad_o(npa),srad_th_ice(npa),srad_ocn(npa),sflux_ocn(npa),fluxprc_ocn(npa),fluxevp_ocn(npa),stat=istat)
-      if(istat/=0) call parallel_abort('INIT: ice frac allocation failure')
-      aice(:)          = real(0)
-      tau_oi(:,:)      = real(0)
-      fresh_wa_flux(:) = real(0)
-      salinity_flux(:) = real(0)
-      net_heat_flux(:) = real(0)
-      srad_o(:)        = real(0)
-      srad_th_ice(:)   = real(0)
+        if(istat/=0) call parallel_abort('INIT: ice frac allocation failure')
+        aice(:)          = real(0)
+        tau_oi(:,:)      = real(0)
+        fresh_wa_flux(:) = real(0)
+        salinity_flux(:) = real(0)
+        net_heat_flux(:) = real(0)
+        srad_o(:)        = real(0)
+        srad_th_ice(:)   = real(0)
       
-      fluxprc_ocn(:)   = real(0)
-      fluxevp_ocn(:)   = real(0)
-      sflux_ocn(:)     = real(0)
-      srad_ocn(:)      = real(0)
+        fluxprc_ocn(:)   = real(0)
+        fluxevp_ocn(:)   = real(0)
+        sflux_ocn(:)     = real(0)
+        srad_ocn(:)      = real(0)
 #endif
-       allocate(sflux_o(npa),stat=istat)
-       sflux_o(:)     = real(0)
+!        allocate(sflux_o(npa),stat=istat)
+!        sflux_o(:)     = real(0)
 
 #ifdef USE_MICE
         allocate(tau_oi(2,npa),fresh_wa_flux(npa),net_heat_flux(npa), &
